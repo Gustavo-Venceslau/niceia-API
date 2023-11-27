e25da6655111bca85df70cf9073df3d8556f4917
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2023-11-27 20:59:13 -0300

    project initialized

---
+++ user/user/src/test/java/com/galmv/entities/UserBuilderTest.java
@@ -0,0 +1,11 @@
+package com.galmv.entities;
+
+import org.junit.jupiter.api.Test;
+
+public class UserBuilderTest {
+
+    @Test
+    public void givenNewUserInformation_whenUserIsBuilt_thenUserIsNotNull(){
+
+    }
+}
